f5ca578a32143ec5fd610c5c5f6ef5c282081adc
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-09-20 15:13:36 +0400

    Add precision for Coordinates

--- src/Services/Warehouse/Dberries.Warehouse.Persistence/Configurations/ValueObjects/CoordinatesConfiguration.cs
+++ src/Services/Warehouse/Dberries.Warehouse.Persistence/Configurations/ValueObjects/CoordinatesConfiguration.cs
@@ -8,7 +8,12 @@ internal static class CoordinatesConfiguration
     public static void Configure<T>(this OwnedNavigationBuilder<T, Coordinates> builder)
         where T : class
     {
-        builder.Property(x => x.Latitude).IsRequired();
-        builder.Property(x => x.Longitude).IsRequired();
+        builder.Property(x => x.Latitude)
+            .HasPrecision(7, 5)
+            .IsRequired();
+        
+        builder.Property(x => x.Longitude)
+            .HasPrecision(7, 5)
+            .IsRequired();
     }
 }
