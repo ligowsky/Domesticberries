bb90baf0b114af15e5ce25de6a0e3cf8d0db6c6e
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-09-20 12:58:10 +0400

    Rename the connection string in Warehouse

--- src/Services/Warehouse/Dberries.Warehouse.Persistence/Extensions/MsSql/AddMsSqlDbContextExtension.cs
+++ src/Services/Warehouse/Dberries.Warehouse.Persistence/Extensions/MsSql/AddMsSqlDbContextExtension.cs
@@ -9,7 +9,7 @@ public static class AddMsSqlDbContextExtension
     public static void AddMsSqlDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<MsSqlDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("Warehouse"),
+            options.UseSqlServer(configuration.GetConnectionString("WarehouseMsSql"),
                 x => { x.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery); }));
 
         services.AddScoped<AppDbContext>(x => x.GetRequiredService<MsSqlDbContext>());
