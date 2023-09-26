978850c123181a7c1a024d5f7420c2e3c9995ae9
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-09-26 15:21:49 +0400

    Add ItemCreated test

--- tests/Services/Dberries.Warehouse.Tests/TestDbContext.cs
+++ tests/Services/Dberries.Warehouse.Tests/TestDbContext.cs
@@ -8,4 +8,10 @@ internal class TestDbContext : AppDbContext
     public TestDbContext(DbContextOptions options) : base(options)
     {
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MsSqlDbContext).Assembly);
+    }
 }
