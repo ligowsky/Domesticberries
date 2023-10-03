05a8652b62550fdf248bb51cadcec04a8eb7c322
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-10-03 11:03:24 +0400

    Fix "UserCreated" test

--- tests/Services/Dberries.Auth.Tests/DbContextTests.cs
+++ tests/Services/Dberries.Auth.Tests/DbContextTests.cs
@@ -21,24 +21,24 @@ public class DbContextTests
         var dbContext = _services.GetRequiredService<AppDbContext>();
 
         //Act
-        var item = new User()
+        var user = new User()
         {
             Id = Guid.NewGuid(),
             Email = "user@dberries.com",
             PasswordHash = "passwordHash"
         };
 
-        await dbContext.AddAsync(item);
+        await dbContext.AddAsync(user);
         await dbContext.SaveChangesAsync();
 
-        var createdItem = await dbContext.Set<User>()
-            .Where(x => x.Id == item.Id)
+        var createdUser = await dbContext.Set<User>()
+            .Where(x => x.Id == user.Id)
             .FirstOrDefaultAsync();
 
         //Assert
-        Assert.NotNull(createdItem);
-        Assert.Equal(item.Id, createdItem.Id);
-        Assert.Equal(item.Email, createdItem.Email);
-        Assert.Equal(item.PasswordHash, createdItem.PasswordHash);
+        Assert.NotNull(createdUser);
+        Assert.Equal(user.Id, createdUser.Id);
+        Assert.Equal(user.Email, createdUser.Email);
+        Assert.Equal(user.PasswordHash, createdUser.PasswordHash);
     }
 }
