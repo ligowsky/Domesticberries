c3919105e4fc8251be2a8f4c319a4735a28faf4c
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-10-06 13:45:57 +0400

    Inject ApiExceptionHandler

--- src/Services/Warehouse/Dberries.Warehouse.WebAPI/Program.cs
+++ src/Services/Warehouse/Dberries.Warehouse.WebAPI/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistence(builder.Configuration);
 
+builder.Services.AddApiExceptionHandler();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
