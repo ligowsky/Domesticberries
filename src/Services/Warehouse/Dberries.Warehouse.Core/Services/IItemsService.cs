91fe6ca4af734d7f351f53137e2a0952b0362850
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-10-06 11:30:54 +0400

    Add IItemsService

---
+++ src/Services/Warehouse/Dberries.Warehouse.Core/Services/IItemsService.cs
@@ -0,0 +1,10 @@
+namespace Dberries.Warehouse;
+
+public interface IItemsService
+{
+    public Task<ICollection<Item>> GetItemsAsync();
+    public Task<Item> GetItemAsync(Guid id);
+    public Task<Item> CreateItemAsync(Item item);
+    public Task<Item> UpdateItemAsync(Guid id, Item item);
+    public Task DeleteItemAsync(Guid id);
+}
--- src/Services/Warehouse/Dberries.Warehouse.WebAPI/Controllers/ItemsController.cs
+++ src/Services/Warehouse/Dberries.Warehouse.WebAPI/Controllers/ItemsController.cs
@@ -6,35 +6,52 @@ namespace Dberries.Warehouse.WebAPI;
 [Route("[Controller]")]
 public class ItemsController : ControllerBase
 {
+    private readonly IItemsService _itemsService;
+
+    public ItemsController(IItemsService itemsService)
+    {
+        _itemsService = itemsService;
+    }
+
     [HttpGet(Name = "GetItems")]
     public async Task<IActionResult> GetItemsAsync()
     {
-        return Ok();
+        var items = await _itemsService.GetItemsAsync();
+
+        return Ok(items);
     }
 
     [HttpGet("{id:guid}", Name = "GetItem")]
-    public async Task<IActionResult> GetItemByIdAsync([FromRoute] Guid id)
+    public async Task<IActionResult> GetItemAsync([FromRoute] Guid id)
     {
-        return Ok();
+        var item = await _itemsService.GetItemAsync(id);
+        
+        return Ok(item);
     }
 
     [HttpPost(Name = "CreateItem")]
     public async Task<IActionResult> CreateItemAsync([FromBody] ItemDto input)
     {
         var item = input.ToModel();
-        return Ok();
+        var createdItem = await _itemsService.CreateItemAsync(item);
+        
+        return Ok(createdItem);
     }
 
     [HttpPut("{id:guid}", Name = "UpdateItem")]
-    public async Task<IActionResult> UpdateItemByIdAsync([FromRoute] Guid id, [FromBody] ItemDto input)
+    public async Task<IActionResult> UpdateItemAsync([FromRoute] Guid id, [FromBody] ItemDto input)
     {
         var item = input.ToModel();
-        return Ok();
+        var updatedItem = await _itemsService.UpdateItemAsync(id, item);
+        
+        return Ok(updatedItem);
     }
 
     [HttpDelete("{id:guid}", Name = "DeleteItem")]
-    public async Task<IActionResult> DeleteItemByIdAsync([FromRoute] Guid id)
+    public async Task<IActionResult> DeleteItemAsync([FromRoute] Guid id)
     {
+        await _itemsService.DeleteItemAsync(id); 
+            
         return Ok();
     }
 }
