5aef3f975145c0ee3b245ffffbb3373062917dca
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-10-27 21:40:05 +0400

    Update ILocationsRepository

--- src/Services/Store/Dberries.Store.Core/Repositories/ILocationsRepository.cs
+++ src/Services/Store/Dberries.Store.Core/Repositories/ILocationsRepository.cs
@@ -7,7 +7,5 @@ public interface ILocationsRepository : IRepository
     public Task<Location> GetAsync(Guid id);
     public Task<Location> Add(Location location);
     public void Remove(Location location);
-
-    public Task<Stock?> GetStockAsync(Guid locationId, Guid itemId);
     public Task<Stock?> UpdateStockAsync(Guid locationId, Guid itemId, int quantity);
 }
