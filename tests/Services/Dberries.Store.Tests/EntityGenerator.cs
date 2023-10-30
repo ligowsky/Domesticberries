cb841cabfb0b20ae8d9aaaa2c24f3743c4cbd1d2
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-10-30 16:28:49 +0400

    Add a testing project for Dberries.Store

---
+++ tests/Services/Dberries.Store.Tests/EntityGenerator.cs
@@ -0,0 +1,31 @@
+namespace Dberries.Store.Tests;
+
+public static class EntityGenerator
+{
+    public static Location GenerateLocation(int number = 1)
+    {
+        return new Location
+        {
+            ExternalId = Guid.NewGuid(),
+            Name = $"Location {number}",
+        };
+    }
+    
+    public static Item GenerateItem(int number = 1)
+    {
+        return new Item
+        {
+            ExternalId = Guid.NewGuid(),
+            Name = $"Item {number}",
+            Description = $"Description {number}"
+        };
+    }
+
+    public static Stock GenerateStock(int quantity = 1)
+    {
+        return new Stock
+        {
+            Quantity = quantity
+        };
+    }
+}
---
+++ tests/Services/Dberries.Store.Tests/TestServiceContainer.cs
@@ -0,0 +1,45 @@
+using Dberries.Store.Infrastructure;
+using Dberries.Store.Persistence;
+using MassTransit;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dberries.Store.Tests;
+
+[CollectionDefinition("Service Collection")]
+public class ContainerCollection : ICollectionFixture<TestServiceContainer> { }
+
+public class TestServiceContainer : IDisposable
+{
+    private readonly IServiceProvider _services;
+    public IServiceProvider ServiceProvider => _services.CreateScope().ServiceProvider;
+
+    public TestServiceContainer()
+    {
+        var services = new ServiceCollection();
+
+        var sqliteConnection = new SqliteConnection("Filename=:memory:");
+        sqliteConnection.Open();
+
+        services.AddDbContext<TestDbContext>(x => x.UseSqlite(sqliteConnection));
+        services.AddScoped<AppDbContext>(x => x.GetRequiredService<TestDbContext>());
+        services.AddServices();
+        services.AddRepositories();
+
+        services.AddMassTransitTestHarness();
+        
+        _services = services.BuildServiceProvider();
+
+        using (var scope = _services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.EnsureCreated();
+        }
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
