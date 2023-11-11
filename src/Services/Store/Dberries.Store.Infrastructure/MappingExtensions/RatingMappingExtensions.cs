bd2eb1adc78750e76449c587e649cfef1ac5df69
Vladimir Seldemirov <ligowsky.dev@gmail.com>
2023-11-12 00:23:11 +0400

    Remove RatingMappingExtensions

--- src/Services/Store/Dberries.Store.Infrastructure/MappingExtensions/RatingMappingExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Dberries.Store;
-
-public static class RatingMappingExtensions
-{
-    public static Rating ToModel(this RatingDto dto, Guid? userId)
-    {
-        return new Rating(userId, dto.Value);
-    }
-}
